9ad4ccbc67201defb3433b371edc72d52b62f31b
Fernando-5D <23308060610105@cetis61.edu.mx>
2025-11-05 16:52:19 -0700

    commit

--- app.py
+++ app.py
@@ -1,5 +1,39 @@
-from flask import Flask
+import datetime
+from flask import Flask, render_template, request, flash, redirect, url_for
 app = Flask(__name__)
+
+sesion = None
+usuarios = {}
+
+@app.route("/sesion")
+def sesion():
+    return render_template("sesion.html")
+
+@app.route("/iniciandoSesion", methods = ("GET", "POST"))
+def iniciandoSesion():
+    if request.method == "POST":
+        correo = request.form.get("correo")
+        for u in usuarios:
+            if correo in usuarios:
+                passw = request.form.get("passw")
+                if passw == usuarios[correo].passw:
+                    sesion = correo
+                else:
+                    flash("La contraseña es incorrecta.")
+                    break
+            else:
+                flash("No se encontro el usuario, ingresaste el correo correctamente?")
+                break
+        
+        return render_template("aqui la url de la plantilla principal")
+
+@app.route("/registro")
+def registro():
+    return render_template("registro.html")
+
+@app.route("/registrando", methods = ("GET", "POST"))
+def registrando():
+    return
 
 if __name__ == "__main__":
     app.run(debug=True)
